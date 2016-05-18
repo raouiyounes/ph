f48a1b176d806d26a173d9e018156d6c3fc55cfb
raouiyounes <younes.fsr@gmail.com>
2016-05-18 16:05:18 +0100

    Update surf.cpp

--- surf.cpp
+++ surf.cpp
@@ -1,46 +1,39 @@
-void  SURFExtractor(IplImage *frame){
- 	 		  int minHessian = 400;
-			  SurfFeatureDetector detector(minHessian);
-	 
-CvMat* image=0;
-
-			   int Threshl=40;
-			   int Octaves=2;
-			   float PatternScales=1.0f;
+int Image::SURFExtractor(IplImage* frame){
+	//  Mat img_1 = image_data;
+	 	  //-- Step 1: Detect the keypoints using SURF Detector
+	  int minHessian = 400;
+	  SurfFeatureDetector detector( minHessian );
+	 	   int Threshl=40;
+	   int Octaves=2;
+	   float PatternScales=1.0f;
+	  CvMat* prevgray = 0, *image = 0, *gray =0;
+//IplImage* frame=cvLoadImage("test_image.jpg");
 image = cvCreateMat(frame->height, frame->width, CV_8UC1);
-			  CvMemStorage* storage = cvCreateMemStorage(0);
-			  cvCvtColor(frame, image, CV_BGR2GRAY);
-			  //Define sequence for storing surf keypoints and descriptors
-			  CvSeq *imageKeypoints = 0, *imageDescriptors = 0;
-			  int i,j;
-		vector<double> descriptor_row;
-			  CvSURFParams params = cvSURFParams(500, 1);
-			  cvExtractSURF( image, 0, &imageKeypoints, &imageDescriptors, storage, params);
-			  MatrixXd descoo(imageDescriptors->total,5);
-			  for( i = 0; i < imageDescriptors->total; i++ )
-			  {
-			double* r = (double*)cvGetSeqElem( imageDescriptors, i);
-				j=0;
-	while(j<5){
-				cout<<*r;
-				descoo(i,j)=*r;
-			//	descriptor_row.push_back(*r);
-			r++;
-			j++;
-			}
-	vector<double> dd1;
-	for(i=1;i<imageDescriptors->total;i++){
-	for(int j=0;j<5;j++)
-		dd1.push_back(descoo(i,j));
-	number_of_line++;
-	dd.push_back(descriptor_row);
-	dd1.clear();
+	  CvMemStorage* storage = cvCreateMemStorage(0);
+	  cvCvtColor(frame, image, CV_BGR2GRAY);
+	  //Define sequence for storing surf keypoints and descriptors
+	  CvSeq *imageKeypoints = 0, *imageDescriptors = 0;
+	  int i,j;
+	  ofstream file("surf.txt");
+vector<double> descriptor_row;
+	  CvSURFParams params = cvSURFParams(500, 1);
+	  cvExtractSURF( image, 0, &imageKeypoints, &imageDescriptors, storage, params );
+	  for( i = 0; i < imageDescriptors->total; i++ )
+	  {
+	float* r = (float*)cvGetSeqElem( imageDescriptors, i);
+	j=0;
+int dim=50;
+	// changer dim pour obtenir plus de features
+	while(j<dim){
+descriptor_row.push_back(*r);
+	r++;
+	j++;
+	file<<*r<<" ";
 	}
-	//descriptor_vect.push_back(descriptor_row);
-	//number_of_line++;
-		//	descriptor_row.clear();
-			  }
-		//	  return dd;
-
-			  descooo=descoo;
+	file<<"\n";
+	descriptor_vect.push_back(descriptor_row);
+	number_of_line++;
+	descriptor_row.clear();
+	  }
+	  return 0;
 }
